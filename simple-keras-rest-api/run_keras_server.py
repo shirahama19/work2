bf556dc18050e872eab77f786ff41ec90a36cd81
白濱 孝紀 <k.shirahama@NP.co.jp>
2019-01-29 17:43:30 +0900

    simple_request.pyの実装

--- simple-keras-rest-api/run_keras_server.py
+++ simple-keras-rest-api/run_keras_server.py
@@ -4,17 +4,21 @@ from keras.applications import ResNet50
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
 from PIL import Image
+import tensorflow as tf
 import numpy as np
 import flask
 import io
 
 
 app = flask.Flask(__name__)
+graph = []
 model = None
 
 def load_model():
+    global graph
     global model
     model = ResNet50(weights = "imagenet")
+    graph = tf.get_default_graph()
 
 
 def prepare_image(image, target):
@@ -44,7 +48,8 @@ def predict():
             image = prepare_image(image, target=(224, 224))
 
             
-            preds = model.predict(image)
+            with graph.as_default():
+                preds = model.predict(image)
             results = imagenet_utils.decode_predictions(preds)
             data["predictions"] = []
 
@@ -61,5 +66,5 @@ if __name__ == "__main__":
     print(("* Loading Keras model and Flask startinf server..."
            "please wait until server has fully started"))
     load_model()
-    app.run()
+    app.run(port=5000)
 
---
+++ simple-keras-rest-api/simple_request.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+KERAS_REST_API_URL = "http://127.0.0.1:5000/predict"
+IMAGE_PATH = "dog.jpg"
+
+
+image = open(IMAGE_PATH, "rb").read()
+payload = {"image":image}
+
+#submit the request
+r = requests.post(KERAS_REST_API_URL, files=payload).json()
+
+#ensure the request was successful
+if r["success"]:
+    for (i, result) in enumerate(r["predictions"]):
+        print("{}. {}: {:.4f}".format(i + 1, result["label"], result["probability"]))
+
+else:
+    print("Request failed")
